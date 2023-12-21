511ec923ffff3e3112f4353422f15e1d4d48701f
Funny_klown <amorroni@protonmail.com>
2023-12-21 15:36:09 +0100

    Fonction convert_csv

--- src/convert.c
+++ src/convert.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 #include "./convert.h"
+#include "./header.h"
 
 
 void convert_csv(FILE* file){
-
+    Header header;
+    processHeader(file,&header);
+    char file_path[150]="./";
+    char file_name[50];
+    //faut la fonction qui stocke les données hors_entête dans un tableau
+    printf("Rentrez le nom du fichier : /n");
+    scanf("%s",file_name);
+    strcat(file_path,file_name);
+    FILE* file_csv = fopen(file_path, "rw");
 };
 
 void convert_png(FILE* file){
 
 };
+ int main(void){    
+    FILE* file =fopen("../lights/r_lights_00001.fit", "r");
+    convert_csv(file);
+    fclose(file);
+    return 0;
+
+ };
